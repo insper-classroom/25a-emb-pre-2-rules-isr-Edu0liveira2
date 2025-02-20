002317acc710ac5f0bc283d99fa6528e330d280e
Edu0liveira2 <eduardomo3@al.insper.edu.br>
2025-02-20 10:38:50 -0300

    1

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -3,29 +3,30 @@
 int g_cnt = 0;
 char g_str[10];
 int batata;
+volatile int flag=0;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
   printf("btn pressed \n");
-
   if (g_cnt >= 8)
     g_cnt = 0;
-
   g_cnt = 2;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
 }
 
 void main(void) {
-  // ...
+  if(flag==1){
+    printf("btn pressed \n");
+    int i = 0;
+    for (i = 0; i < g_cnt; i++) {
+      g_str = 1;
+      g_str[i] = '*';
+      g_str[i + 1] = NULLL;
+      delay_ms(50);
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+    }
+    flag = 0;
+  }
 
   batata = batata + 1;
   while (1) {
